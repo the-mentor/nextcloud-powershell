f4c73b9192046ee11ed6fda8b13e3e4a1309a3ee
ili101 <zoby101@gmail.com>
2019-10-06 22:06:19 +0300

    Pester tests

--- Tests/Users.Tests.ps1
+++ Tests/Users.Tests.ps1
@@ -1,26 +1,50 @@
 #Requires -Modules Pester
-[CmdletBinding(DefaultParameterSetName = 'Default', SupportsShouldProcess, ConfirmImpact = 'Low')]
+[CmdletBinding()]
 param (
     [PSCredential]$Credential = $NextcloudCredential,
     [string]$Server = $NextcloudServer
 )
-
-$NextcloudCredential = Get-Credential
-$NextcloudServer = Read-Host -Prompt 'Nextcloud Server'
-
-Describe 'Test-Q' {
-    It 'Returns Q' {
+if ($env:AGENT_NAME) {
+    $Credential = $(NextcloudCredential)
+    $Server = $(NextcloudServer)
+}
+else {
+    if (!$Credential) {
+        $Credential = $Global:NextcloudCredential = Get-Credential
+    }
+    if (!$Server) {
+        $Server = $Global:NextcloudServer = Read-Host -Prompt 'Nextcloud Server'
     }
 }
 
-#<#
-$User = 'Me'
-$Credential = Get-Credential $User
+Describe 'Users' {
+    $UserId = $Credential.UserName
+    It 'Connect-NextcloudServer' {
+        Connect-NextcloudServer -Server $Server -Credential $Credential | Should -BeNullOrEmpty
+    }
+    It 'Get-NextcloudUser' {
+        $User = Get-NextcloudUser -UserID $UserId | Should -BeNullOrEmpty
+        $User.id | Should -Be $UserId
+    }
+    It 'Add-NextcloudUser' {
+        Remove-NextcloudUser -UserID "$UserId-Test1" -ErrorAction SilentlyContinue
+        Add-NextcloudUser -UserID "$UserId-Test1" -Password New-Guid | Should -BeNullOrEmpty
+        (Get-NextcloudUser -UserID $UserId).id | Should -Be "$UserId-Test1"
 
-Connect-NextcloudServer -Server nextcloud-dev.powershellonlinux.com -Credential $Credential
-
-Get-NextcloudUser -UserID $User
-Set-NextcloudUser -Email 'zoby101@gmail.com' -UserID $User
-Add-NextcloudUser -UserID "$User-Test1" -Password New-Guid
-Remove-NextcloudUser -UserID "$User-Test1"
-#>
+        { Add-NextcloudUser -UserID "$UserId-Test1" -Password New-Guid } | Should -Throw -ExpectedMessage 'User already exists'
+    }
+    It 'Get-NextcloudUsers' {
+        $Users = Get-NextcloudUser
+        $Users.id | Should -Contain $UserId
+        $Users.id | Should -Contain "$UserId-Test1"
+    }
+    It 'Set-NextcloudUser' {
+        Set-NextcloudUser -UserID "$UserId-Test1" -Email 'me@example.com' | Should -BeNullOrEmpty
+        (Get-NextcloudUser -UserID "$UserId-Test1").email | Should -Be 'me@example.com'
+    }
+    It 'Remove-NextcloudUser' {
+        Remove-NextcloudUser -UserID "$UserId-Test1" | Should -BeNullOrEmpty
+        { Remove-NextcloudUser -UserID "$UserId-Test1" } | Should -Throw -ExpectedMessage '101'
+        Get-NextcloudUser -UserID "$UserId-Test1" | Should -BeNullOrEmpty
+    }
+}
